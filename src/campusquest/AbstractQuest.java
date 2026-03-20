55594934c92ec00b15774b0f0e36343ae0a230ef
k3ppl3r <bnknqh@umsystem.edu>
2026-03-19 19:09:51 -0500

    Update AbstractQuest.java

--- src/campusquest/AbstractQuest.java
+++ src/campusquest/AbstractQuest.java
@@ -25,7 +25,7 @@ public abstract class AbstractQuest implements Quest {
         this.completed = false;
     }
 
-    //call this once the quest is finished; can't go back
+    //call this once the quest is finished; can't go back.
     protected void markCompleted() {
         if (completed)
             throw new IllegalStateException("Quest " + id + " already completed");
